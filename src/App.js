c8346fa5e94dd553721efc4ca8aed4e2d73f41f2
Sinjin Jang <sinjin@stronghold.kr>
2021-02-16 17:20:18 +0900

    키 스위치 및 키 배치판 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// 기본 1U 스위치의 크기 정의 (px 단위)
+const UNIT_1 = 50;
+
+class KeySwitch extends React.Component {
+  render() {
+    const style = {
+      left: (this.props.x * UNIT_1),
+      top: (this.props.y * UNIT_1),
+      width: (this.props.w * UNIT_1) - 2,  // NOTE: 테두리 두께만큼 빼줌
+      height: (this.props.h * UNIT_1) - 2,  // NOTE: 테두리 두께만큼 빼줌
+    };
+    return (
+      <div className="key-switch" style={style}>
+        {this.props.label}
+      </div>
+    )
+  }
+}
+
+class KeyPlate extends React.Component {
+  render() {
+    const plateWidth = 2 + 1.75; // TODO: 가장 오른쪽 키의 x + w 계산
+    const plateHeight = 2 + 1;  // TODO: 가장 아래쪽 키의 y + h 계산
+
+    const MARGIN_PLATE = 30;
+    const style = {
+      width: (plateWidth * UNIT_1) + MARGIN_PLATE,
+      height: (plateHeight * UNIT_1) + MARGIN_PLATE,
+    };
+    const styleInner = {
+      width: (plateWidth * UNIT_1),
+      height: (plateHeight * UNIT_1),
+    };
+    return (
+      <div className="key-plate-outer" style={style}>
+        <div className="key-plate-inner" style={styleInner}>
+          <KeySwitch label="a" x="0" y="0" w="1" h="1" />
+          <KeySwitch label="b" x="1" y="0" w="1" h="1" />
+          <KeySwitch label="c" x="2" y="0" w="1.25" h="1" />
+          <KeySwitch label="d" x="0" y="1" w="1" h="1" />
+          <KeySwitch label="e" x="1" y="1" w="1" h="1" />
+          <KeySwitch label="f" x="2" y="1" w="1.5" h="1" />
+          <KeySwitch label="g" x="0" y="2" w="1" h="1" />
+          <KeySwitch label="h" x="1" y="2" w="1" h="1" />
+          <KeySwitch label="i" x="2" y="2" w="1.75" h="1" />
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <KeyPlate />
     </div>
   );
 }
