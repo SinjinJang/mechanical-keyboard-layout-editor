5cfbf0aeeb8ce3f4317ac8117dc4ccee66ccc9c4
Sinjin Jang <sinjin@stronghold.kr>
2021-02-17 17:46:22 +0900

    상판 넓이 높이 값을 state로 저장함

--- src/App.js
+++ src/App.js
@@ -21,18 +21,30 @@ class KeySwitch extends React.Component {
 }
 
 class KeyPlate extends React.Component {
-  render() {
-    const plateWidth = 2 + 1.75; // TODO: 가장 오른쪽 키의 x + w 계산
-    const plateHeight = 2 + 1;  // TODO: 가장 아래쪽 키의 y + h 계산
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      width: 2+1.75,  // TODO: 가장 오른쪽 키의 x + w 계산
+      height: 2+1,  // TODO: 가장 아래쪽 키의 y + h 계산
+      layout: []
+    };
+  }
+
+  handleAddSwitch(e) {
+    console.log('handleAddSwitch');
+  }
+
+  render() {
     const MARGIN_PLATE = 30;
     const style = {
-      width: (plateWidth * UNIT_1) + MARGIN_PLATE,
-      height: (plateHeight * UNIT_1) + MARGIN_PLATE,
+      width: (this.state.width * UNIT_1) + MARGIN_PLATE,
+      height: (this.state.height * UNIT_1) + MARGIN_PLATE,
     };
     const styleInner = {
-      width: (plateWidth * UNIT_1),
-      height: (plateHeight * UNIT_1),
+      width: (this.state.width * UNIT_1),
+      height: (this.state.height * UNIT_1),
     };
     return (
       <div className="key-plate-outer" style={style}>
