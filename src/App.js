4fb91508d74067b0c5db91f391a7d6d1466a00ef
Sinjin Jang <sinjin@stronghold.kr>
2021-02-18 17:10:29 +0900

    스위치 정의 객체에 w(width)와 h(height)를 항상 지정하도록 변경

--- src/App.js
+++ src/App.js
@@ -40,23 +40,23 @@ class KeyPlate extends React.Component {
       width: 2 + 1,  // TODO: 가장 오른쪽 키의 x + w 계산
       height: 5.25 + 1,  // TODO: 가장 아래쪽 키의 y + h 계산
       layout: [
-        { 'key': 'Print Screen', 'x': 0, 'y': 0 },
-        { 'key': 'Scroll Lock', 'x': 1, 'y': 0 },
-        { 'key': 'Pause', 'x': 2, 'y': 0 },
+        { 'key': 'Print Screen', 'x': 0, 'y': 0, 'w': 1, 'h': 1 },
+        { 'key': 'Scroll Lock', 'x': 1, 'y': 0, 'w': 1, 'h': 1 },
+        { 'key': 'Pause', 'x': 2, 'y': 0, 'w': 1, 'h': 1 },
 
-        { 'key': 'Ins', 'x': 0, 'y': 1.25 },
-        { 'key': 'Home', 'x': 1, 'y': 1.25 },
-        { 'key': 'Page Up', 'x': 2, 'y': 1.25 },
+        { 'key': 'Ins', 'x': 0, 'y': 1.25, 'w': 1, 'h': 1 },
+        { 'key': 'Home', 'x': 1, 'y': 1.25, 'w': 1, 'h': 1 },
+        { 'key': 'Page Up', 'x': 2, 'y': 1.25, 'w': 1, 'h': 1 },
 
-        { 'key': 'Delete', 'x': 0, 'y': 2.25 },
-        { 'key': 'End', 'x': 1, 'y': 2.25 },
-        { 'key': 'Page Down', 'x': 2, 'y': 2.25 },
+        { 'key': 'Delete', 'x': 0, 'y': 2.25, 'w': 1, 'h': 1 },
+        { 'key': 'End', 'x': 1, 'y': 2.25, 'w': 1, 'h': 1 },
+        { 'key': 'Page Down', 'x': 2, 'y': 2.25, 'w': 1, 'h': 1 },
 
-        { 'key': 'Up', 'x': 1, 'y': 4.25 },
+        { 'key': 'Up', 'x': 1, 'y': 4.25, 'w': 1, 'h': 1 },
 
-        { 'key': 'Left', 'x': 0, 'y': 5.25 },
-        { 'key': 'Down', 'x': 1, 'y': 5.25 },
-        { 'key': 'Right', 'x': 2, 'y': 5.25 }
+        { 'key': 'Left', 'x': 0, 'y': 5.25, 'w': 1, 'h': 1 },
+        { 'key': 'Down', 'x': 1, 'y': 5.25, 'w': 1, 'h': 1 },
+        { 'key': 'Right', 'x': 2, 'y': 5.25, 'w': 1, 'h': 1 }
       ]
     };
   }
@@ -105,8 +105,7 @@ class KeyPlate extends React.Component {
           key={idx + '-' + val.key}
           label={val.key}
           x={val.x} y={val.y}
-          w={val.w !== undefined ? val.w : 1}
-          h={val.h !== undefined ? val.h : 1}
+          w={val.w} h={val.h}
         />
       )
     }
