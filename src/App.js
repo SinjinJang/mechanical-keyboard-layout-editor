44478694857fb3c7e3813a9fa04cbe8b9861c771
Sinjin Jang <sinjin@stronghold.kr>
2021-05-10 15:22:41 +0900

    KeyPlate를 별도 컴포넌트 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
 import './App.css';
-import React from 'react';
-import { useState } from '@hookstate/core';
 
-import * as LayoutUtil from './utils/LayoutUtil';
-import EditPanel from './components/EditPanel';
-import KeySwitch from './components/KeySwitch';
+import KeyPlate from './components/KeyPlate';
 import Footer from './components/Footer';
 
-
-function KeyPlate() {
-  const layoutState = useState([
-    { label: 'Key 1', w: 1, h: 1, x: 0, y: 0 },
-  ]);
-  const selectedState = useState(-1);
-
-  return (
-    <div>
-      <EditPanel
-        layoutState={layoutState}
-        selectedState={selectedState}
-      />
-      <div
-        className='key-plate'
-        style={LayoutUtil.plateSize(layoutState.get())}>
-        {layoutState.map((keyState, index) =>
-          <KeySwitch
-            key={index}
-            seq={index}
-            keyState={keyState}
-            selectedState={selectedState}
-          />
-        )}
-      </div>
-    </div>
-  )
-}
 
 function App() {
   return (
---
+++ src/components/KeyPlate.js
@@ -0,0 +1,38 @@
+import './KeyPlate.css';
+
+import { useState } from '@hookstate/core';
+
+import { plateSize } from '../utils/LayoutUtil';
+import EditPanel from './EditPanel';
+import KeySwitch from './KeySwitch';
+
+
+function KeyPlate() {
+  const layoutState = useState([
+    { label: 'Key 1', w: 1, h: 1, x: 0, y: 0 },
+  ]);
+  const selectedState = useState(-1);
+
+  return (
+    <div>
+      <EditPanel
+        layoutState={layoutState}
+        selectedState={selectedState}
+      />
+      <div
+        className='key-plate'
+        style={plateSize(layoutState.get())}>
+        {layoutState.map((keyState, index) =>
+          <KeySwitch
+            key={index}
+            seq={index}
+            keyState={keyState}
+            selectedState={selectedState}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default KeyPlate;
