881b505dcda2e07f3d16f880ad5908f1c59f0c3e
Sinjin Jang <sinjin@stronghold.kr>
2021-05-14 09:45:29 +0900

    class -> className 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <header>
-      <p class='app_name'>Mechanical Keyboard Layout Editor</p>
+      <p className='app_name'>Mechanical Keyboard Layout Editor</p>
     </header>
   );
 }
