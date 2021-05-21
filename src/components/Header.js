fc4f8fe70f79058e6be4c0867bbfd55a68f4c67b
Sinjin Jang <sinjin@stronghold.kr>
2021-05-21 11:51:13 +0900

    코드 정리: arrow function으로 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,11 @@ import './Header.css';
 
 import AboutDialog from './AboutDialog';
 
-function Header() {
-  return (
-    <header>
-      <p className='app_name'>Mechanical Keyboard Layout Editor</p>
-      <AboutDialog />
-    </header>
-  );
-}
+const Header = () => (
+  <header>
+    <p className='app_name'>Mechanical Keyboard Layout Editor</p>
+    <AboutDialog />
+  </header>
+);
 
 export default Header;
