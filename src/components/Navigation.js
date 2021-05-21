233ebf8cf67034f301dd193c428555dc74ec4517
Sinjin Jang <sinjin@stronghold.kr>
2021-05-21 11:05:53 +0900

    nav bar 스타일 지정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,9 @@
+import './Navigation.css';
+
 import { Link } from 'react-router-dom';
 
 const Navigation = () => (
-  <div>
+  <div className='nav'>
     <Link to='/'>Home</Link>
     <Link to='/about'>About</Link>
   </div>
